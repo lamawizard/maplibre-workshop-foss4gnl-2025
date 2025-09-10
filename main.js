a5b2adf68a6148710773bed3bdfd788efe7403b2
lamawizard <146738404+lamawizard@users.noreply.github.com>
2025-09-10 13:39:27 +0000

    pmtiles install

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+import { Protocol } from "https://esm.sh/pmtiles";
+const protocol = new Protocol();
+maplibregl.addProtocol("pmtiles", protocol.tile);
+
 console.log("Ook hallo wereld vanuit javascript")
 
 import * as maplibregl from "https://esm.sh/maplibre-gl";
 
 const map = new maplibregl.Map({
     container: 'mijnkaart', // container id
-    style: 'https://demotiles.maplibre.org/style.json', // style URL
-    center: [0, 0], // starting position [lng, lat]
-    zoom: 1 // starting zoom
+    style: 'style: /assets/style.json', // style URL
+    center: [51.96857, 5.66509], // starting position [lng, lat]
+    zoom: 13 // starting zoom
 });
