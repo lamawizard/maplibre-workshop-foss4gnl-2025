4328669947c9cd18063f13d58738c6f680e507b2
lamawizard <146738404+lamawizard@users.noreply.github.com>
2025-09-15 10:59:28 +0000

    import van maplibregl naar boven geplaats

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+import * as maplibregl from "https://esm.sh/maplibre-gl";
 import { Protocol } from "https://esm.sh/pmtiles";
+
 const protocol = new Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
 
 console.log("Ook hallo wereld vanuit javascript")
 
-import * as maplibregl from "https://esm.sh/maplibre-gl";
 
 const map = new maplibregl.Map({
     container: 'mijnkaart', // container id
