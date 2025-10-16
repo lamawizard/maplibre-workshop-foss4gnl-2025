93f0f5da140008e1b88c526635c24b560f241522
lamawizard <146738404+lamawizard@users.noreply.github.com>
2025-10-16 11:18:55 +0000

    wandeling.geojson toevoegen aan main.js voor laag in kaart

--- main.js
+++ main.js
@@ -7,7 +7,28 @@ maplibregl.addProtocol("pmtiles", protocol.tile);
 
 const map = new maplibregl.Map({
       container: 'mijnkaart', // id van de div in index.html
-        style: '/assets/style.json',
+        style:  "https://tiles.openfreemap.org/styles/liberty",
           center: [ 5.66509 ,51.96857 ], 
             zoom:  13
             });
+
+fetch("assets/wandeling.geojson")
+.then(response => response.json())
+.then(geojson => {
+    map.on('load', () => {
+        map.addSource('wandeling', {
+            'type': 'geojson',
+            'data': geojson
+        });
+        map.addLayer({
+            'id': 'wandeling',
+            'type': 'line',
+            'source': 'wandeling',
+            'layout': {},
+            'paint': {
+                'line-color': '#FF0000',
+                'line-width': 4
+            }
+        });
+    });
+});
